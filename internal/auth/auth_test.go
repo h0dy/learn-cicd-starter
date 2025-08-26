b09d8599adce506e7f3c7b3b101918d5a0c9910d
h0dy <hasolanh@gmail.com>
2025-08-27 01:36:51 +0300

    unformatted code

--- internal/auth/auth_test.go
+++ internal/auth/auth_test.go
@@ -5,58 +5,64 @@ import (
 	"testing"
 )
 
-func TestGetAPIKey(t *testing.T) {
+func TestGetAPIKey(t *testing.T)    {
 	cases := []struct {
-		name string
-		header http.Header
-		output string
-		expectErr bool
-	}{
+		name      string
+		header    http.Header
+		output    string
+		expectErr bool}{
 		{
-			name: "Valid api key",
-			header: http.Header{"Authorization": []string{"ApiKey randomApiKey"}},
-			output: "randomApiKey",
+			name:      "Valid api key",
+			header:    http.Header{"Authorization": []string{"ApiKey randomApiKey"}},
+			output:    "randomApiKey",
 			expectErr: false,
 		},
 		{
-			name: "invalid prefix",
-			header: http.Header{"Authorization": []string{"auth lsajdf9f7sd;k"}},
-			output: "lsajdf9f7sd;k",
+			name:      "invalid prefix",
+			header:    http.Header{"Authorization": []string{"auth lsajdf9f7sd;k"}},
+			output:    "lsajdf9f7sd;k",
 			expectErr: true,
 		},
 		{
-			name: "valid api key 2",
-			header: http.Header{"Authorization": []string{"ApiKey als;dkjfjsalkdfjff97vc"}},
-			output: "als;dkjfjsalkdfjff97vc",
+			name:      "valid api key 2",
+			header:    http.Header{"Authorization": []string{"ApiKey als;dkjfjsalkdfjff97vc"}},
+			output:    "als;dkjfjsalkdfjff97vc",
 			expectErr: false,
 		},
 		{
-			name: "missing authorization header",
-			header: http.Header{"NotAuthorizationHeader": []string{"ApiKey sdf7890qwehjklo234hjkl"}},
-			output: "sdf7890qwehjklo234hjkl",
+			name:      "missing authorization header",
+			header:    http.Header{"NotAuthorizationHeader": []string{"ApiKey sdf7890qwehjklo234hjkl"}},
+			output:    "sdf7890qwehjklo234hjkl",
 			expectErr: true,
 		},
 		{
-			name: "missing prefix",
-			header: http.Header{"Authorization": []string{"notApiKey"}},
-			output: "idk",
+			name:      "missing prefix",
+			header:    http.Header{"Authorization": []string{"notApiKey"}},
+			output:    "idk",
 			expectErr: true,
 		},
 	}
 
 	for _, c := range cases {
-		t.Run(c.name, func(t *testing.T) {
+		t.Run(c.name, func(t *testing.T)                {
 			got, err := GetAPIKey(c.header)
-
+			
+			
+			
+			
+			
+			
 			if err != nil {
 				if !c.expectErr {
 					t.Errorf("unexpected error: %v\n", err)
 				}
-				return 
+				return
 			}
 
 			if c.expectErr {
 				t.Error("expected error but got none case")
+				
+				
 				return
 			}
 
